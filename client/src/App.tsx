431f0a41a15f4245a25065205af68bf7f5c66c4a
JanineLapiz <janine@restoplus.com.au>
2025-07-12 20:46:18 +0800

    Reconfigure prettierrc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client';
 const rootNode = document.getElementById('root');
 
 if (rootNode) {
- const root = createRoot(rootNode);
+  const root = createRoot(rootNode);
 
- const App = (): JSX.Element => {
-  return <div className="app">Hello</div>;
- };
+  const App = (): JSX.Element => {
+    return <div className="app">Hello</div>;
+  };
 
- root.render(<App />);
+  root.render(<App />);
 }
 
 console.error(`rootNode is undefined in App.tsx`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 
 const model = new ChatGoogleGenerativeAI({
- model: 'gemini-2.0-flash',
- temperature: 0
+  model: 'gemini-2.0-flash',
+  temperature: 0
 });
