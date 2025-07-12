170e7a19622828da5fd7273e7b4f2563cd4a4b6e
JanineLapiz <janine@restoplus.com.au>
2025-07-12 16:53:44 +0800

    Set up repo base

---
+++ README.md
@@ -0,0 +1,11 @@
+## Local Development Setup
+
+1. Install PNPM
+
+   npm i -g pnpm@7.13.0
+
+2. Set up dependencies
+
+   pnpm i
+
+3. Add Prettier in your VS Code extension if you're want code formatter during editing.
---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { JSX } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const rootNode = document.getElementById('root');
+
+if (rootNode) {
+ const root = createRoot(rootNode);
+
+ const App = (): JSX.Element => {
+  return <div className="app">Hello</div>;
+ };
+
+ root.render(<App />);
+}
+
+console.error(`rootNode is undefined in App.tsx`);
